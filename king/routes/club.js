004a38456515df580ad83f18a3c0b1a4136a7fa0
bong44 <qhdrud0429@gmail.com>
2022-03-14 16:58:21 +0900

    m

--- king/routes/club.js
+++ king/routes/club.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   if(!req.session.user){
     //로그인 안 됨
-    res.render('main',{user:"N"});
+    res.render('',{user:"N"});
   }else{
     //로그인 됨
-    res.render('main',{user: req.session.user.data});
+    res.render('',{user: req.session.user.data});
   }
 });
 
